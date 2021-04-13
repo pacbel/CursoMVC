a402889b1c6a01a380f4816d7051213a70aaad40
pacbel <suporte@sistemavirtual.com.br>
2021-04-13 18:16:34 -0300

    202104131734

---
+++ CursoTeste/CategoriasControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoTeste
+{
+    public class CategoriasControllerTest
+    {
+        public CategoriasControllerTest()
+        {
+
+        }
+
+
+    }
+}
